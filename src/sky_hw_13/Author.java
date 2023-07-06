51c40dd711261e4ab648bdb8448eb267c9c27fee
NN <nik_rink@mail.ru>
2023-07-07 00:17:11 +0300

    Сделал ДЗ-13,задачи 1-1(на основе ДЗ-12), поместил в hw13 ветку... Перопределил методы toString(), equals(), hashCode()... Сделал проверку в Main...

--- src/sky_hw_13/Author.java
+++ src/sky_hw_13/Author.java
@@ -1,19 +1,41 @@
 package sky_hw_13;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String family;
 
-    public  Author(String name, String family){
+    public Author(String name, String family) {
         this.name = name;
         this.family = family;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public String getFamily(){
+    public String getFamily() {
         return this.family;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя автора = " +
+                name + "\n" +
+                "Фамилия автора = " + family + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(family, author.family);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, family);
     }
 }
